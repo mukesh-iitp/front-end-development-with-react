cade55a7aa47724b9ef8998b8265c1fc45581a28
Mukesh Kr <146891811+mukesh-iitp@users.noreply.github.com>
2025-11-02 20:36:35 +0530

    UnderstandLifeCycle
    Understanding the Life Cycle of React Component

---
+++ UnderstandLifeCycle/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ComponentA from "./ComponentA";
+
+class App extends React.Component{
+  render(){
+    return(
+      <>
+      <ComponentA/>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ UnderstandLifeCycle/src/ComponentA.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class ComponentA extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            name: "ComponentA"
+        }
+
+        console.log("ComponentA constructor")
+ 
+        /*
+        this.setState(
+            {name:"setState() called in ComponentA constructor()"}
+        )// Can't call setState on a component that is not yet mounted.
+        */
+    }
+
+    //static getDerivedStateFromProps(porps,state){
+    static getDerivedStateFromProps(){
+        console.log("ComponentA getDerivedStateByProps")
+        /*
+        this.setState(
+            {name : "setState() called in ComponentA getDerivedStateFromProps()"}
+        )//uncaught TypeError: Cannot read properties of undefined (reading 'setState')
+        */
+        return null;
+    }
+
+    componentDidMount(){
+        console.log("ComponentA componentDidMount")
+        this.setState(
+            {name : "setState() called in ComponentA componentDidMount()"}
+        )
+    }
+
+    render(){
+        console.log("ComponentA render")
+        /*
+        this.setState(
+            {name : "setState() called in render()"}
+        )// infinit loop because setState() implicitly call render function
+        */
+        return(
+            <h1>{this.state.name}</h1>
+        )
+    }
+}
+
+export default ComponentA;
