92bd74ed392792842e6e72f08eabffe866fb7ec8
Mukesh Kr <146891811+mukesh-iitp@users.noreply.github.com>
2025-11-03 14:56:02 +0530

    digital image gallery app
    digital image gallery app

---
+++ digital image gallery app/src/App.js
@@ -0,0 +1,49 @@
+import "./styles.css";
+import React from "react";
+import Image from "./components/Image";
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: [],   // State to store fetched photos
+      loading: false   // State to manage loading status
+    };
+  }
+
+  // Use the required lifecycle methods here
+  // Make an API call to fetch images and update state accordingly
+  // Ensure that loading is set to true before the API request and false after data is fetched
+
+  componentDidMount(){
+        console.log("ComponentA componentDidMount")
+
+        this.setState({loading: true})
+
+        fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+            .then(response => response.json())
+            .then(data => this.setState({photos : data, loading: false }))
+
+        //this.setState({loading: false})
+        
+    }
+
+  render() {
+
+    const {photos, loading} = this.state;
+
+    // Display loading status here
+    // If loading is true, display the message "Loading..."
+    
+    if(loading)
+      return(<h1>Loading...</h1>)
+
+    return (
+      <div className="App">
+        {photos.map((photo) => {
+          return <Image key={photo.id} photo={photo} />;
+        })}
+      </div>
+    );
+  }
+}
